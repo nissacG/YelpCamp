0eed4a6f5e1cfefa8d3267264133b1d832ed779b
Gareth Cassin <nissacjg@gmail.com>
2018-02-06 12:26:25 +0000

    add mlab

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_12");
+// mongoose.connect("mongodb://localhost/yelp_camp_12");
+mongoose.connect("mongodb://nissacjg:6Railwayst@ds125288.mlab.com:25288/nissacjg-yelpcampv12");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
